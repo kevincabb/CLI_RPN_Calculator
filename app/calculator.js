ed68a9bf1e4769ff2227b76fee4655ec9ea1838b
kevincabb <kev1ncabb@gmail.com>
2022-07-31 15:11:07 -0700

    Initialize Calculator App

---
+++ app/calculator.js
@@ -0,0 +1,16 @@
+class Calculator {
+  constructor() {}
+
+  initCalculator() {}
+
+  evaluateInput() {}
+
+  checkEOF() {}
+
+  processCalculation() {}
+
+  operatorCase() {}
+}
+
+const calc = new Calculator();
+calc.initCalculator();
