b59b6d53d5ebb56a6fffeec9e2aec615aaae0ebe
kevincabb <kev1ncabb@gmail.com>
2022-07-31 15:54:44 -0700

    Define Calculator Enums

---
+++ app/enums.js
@@ -0,0 +1,15 @@
+exports.Operator = {
+  Addition: "+",
+  Subtraction: "-",
+  Multiplication: "*",
+  Division: "/",
+};
+
+exports.CalcConstant = {
+  MinOperandStackLength: 2,
+};
+
+exports.CalcAppAction = {
+  End: "q",
+  Clear: "c",
+};
